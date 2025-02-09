22ecf1b6e7ec3b49e010f4eaf05ee75756cb2e8d
parthDev <shaakaal00001@gmail.com>
2025-02-09 11:36:13 +0530

    Deployment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUP'
+import Home from './pages/home/home'
+import { Route,Routes } from 'react-router-dom'
+import { Toaster } from "react-hot-toast"
+import { useAuthContext } from './context/AuthContext'
+import { Navigate } from 'react-router-dom'
+
+function App() {
+  const {authUser} = useAuthContext();
+
+  return (
+   <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path='/' element={authUser?<Home/>:<Navigate to={'/login'}/> } />
+        <Route path='/login' element={authUser? <Navigate to='/' />: <Login/>} />
+        <Route path='/signup' element={authUser? <Navigate to='/' />: <SignUp/>} />
+      </Routes>
+      <Toaster/>
+   </div>
+  )
+}
+
+export default App;
